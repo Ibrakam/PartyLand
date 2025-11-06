003b1f92c8549da770704e2f63afcb63be69df27
Ibragim <72756511+Ibrakam@users.noreply.github.com>
2025-11-06 16:52:29 +0500

    frontendweb

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -49,6 +62,12 @@ const nextConfig = {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '81.162.55.70',
+        port: '8001',
         pathname: '/media/**',
       },
     ],
